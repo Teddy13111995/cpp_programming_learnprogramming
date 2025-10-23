354213c4679b31ce5d7ee232f0a87a9e975963d0
Teddy13111995 <34350921+Teddy13111995@users.noreply.github.com>
2025-10-23 20:16:59 +0530

    exceptionHandling

---
+++ mpgException.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int miles{};
+    int gallons{};
+    double miles_per_gallons{};
+
+    cout<<"Enter the miles: ";
+    cin>>miles;
+
+    cout<<"Enter the gallons: ";
+    cin>>gallons;
+    try{
+        if(gallons==0)
+            throw 0;
+        miles_per_gallons=static_cast<double> (miles)/gallons;
+        cout<<"Result: "<<miles_per_gallons<<endl;
+    }
+    catch(int &ex){
+        cerr<<"Sorry, cant divide by zero"<<endl;
+    }
+    cout<<"Bye"<<endl;
+    return 0;
+}
